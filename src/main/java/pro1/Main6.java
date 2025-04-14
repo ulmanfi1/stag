a22815726603c2db6d21340cd22eb21b1fcfc84b
ulmanfi1 <ulmanfi1@uhk.cz>
2025-04-14 17:22:39 +0200

    All test passed!

--- src/main/java/pro1/Main6.java
+++ src/main/java/pro1/Main6.java
@@ -1,6 +1,11 @@
 package pro1;
 
+import com.google.gson.Gson;
 import pro1.apiDataModel.ActionsList;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Locale;
 
 public class Main6 {
 
@@ -10,10 +15,20 @@ public class Main6 {
 
     public static long idOfBestTeacher(String department, int year)
     {
+        String json = Api.getActionsByDepartment(department,year);
+        ActionsList actions= new Gson().fromJson(json, ActionsList.class);
+        HashMap<Long, Integer> hashMap= new HashMap<>();
+        actions.items.stream().forEach(a -> {
+            int current = hashMap.getOrDefault(a.teacherId,0);
+            hashMap.put(a.teacherId, current+ a.personsCount);
+            });
+        return hashMap.entrySet().stream()
+                .max(Comparator.comparing(h->h.getValue()))
+                .get()
+                .getKey();
+
         // TODO 6.1 (navazuje na TODO 3):
         //  - Stáhni seznam akcí na katedře (jiná data nepoužívat)
         //  - Najdi učitele s nejvyšším "score" a vrať jeho ID
-
-        return 0;
     }
 }
